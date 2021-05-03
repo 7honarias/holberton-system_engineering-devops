4bc6c44b485a4570d9a7c0f34a2eb110ee34067a
7honarias <7honarias@gmail.com>
2021-05-03 23:31:39 +0000

    [UPDATE] - correction gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ def api_show(argv):
     id_user = str(argv[1])
     number_of_done_tasks = 0
     total_number_of_tasks = 0
-    task_title = ""
+    task_title = []
 
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id_user)
     user = requests.get(user_url).json()
@@ -20,13 +20,14 @@ def api_show(argv):
     for task in tasks:
         if (task['userId'] == int(id_user)) and (task['completed'] is True):
             number_of_done_tasks += 1
-            task_title += "\t {}\n".format(task['title'])
-        elif task['userId'] == int(id_user):
+            task_title.append(task['title'])
+        if task['userId'] == int(id_user):
             total_number_of_tasks += 1
 
     print("Employee {} is done with task({}/{}):"
         .format(employee_name, number_of_done_tasks, total_number_of_tasks))
-    print('{}'.format(task_title), end='')
+    for title in task_title:
+        print('\t {}'.format(title))
 
 if __name__ == '__main__':
     api_show(argv)
