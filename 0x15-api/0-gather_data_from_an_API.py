46f4f1999b3345649b1d738c7aa53218b60645b4
7honarias <7honarias@gmail.com>
2021-05-03 22:52:00 +0000

    [ADD] - script that using this api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that using REST API"""
+import requests
+from sys import argv
+
+
+id_user = str(argv[1])
+number_of_done_tasks = 0
+total_number_of_tasks = 0
+task_title = ""
+
+user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id_user)
+user = requests.get(user_url).json()
+employee_name = user['name']
+
+task_url = 'https://jsonplaceholder.typicode.com/todos'
+tasks = requests.get(task_url).json()
+for task in tasks:
+    if (task['userId'] == int(id_user)) and (task['completed'] is True):
+        number_of_done_tasks += 1
+        task_title += "\t {}\n".format(task['title'])
+    elif task['userId'] == int(id_user):
+        total_number_of_tasks += 1
+
+print("Employee {} is done with task({}/{}):"
+      .format(employee_name, number_of_done_tasks, total_number_of_tasks))
+print('{}'.format(task_title))
