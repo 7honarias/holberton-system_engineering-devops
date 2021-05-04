77031d608610411a30cd80377013fd839163a91e
7honarias <7honarias@gmail.com>
2021-05-04 00:46:34 +0000

    [ADD] - exercise json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that using REST API"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    file_json = argv[1] + '.json'
+    list_task = []
+    dir_task = {}
+    for task in todo:
+        dir_task['task'] = task.get('title')
+        dir_task['completed'] = task.get('completed')
+        dir_task['username'] = task.get('username')
+        list_task.append(dir_task)
+
+    with open(file_json, 'w', newline='') as jsonfile:
+        json.dump({argv[1]: list_task}, jsonfile)
