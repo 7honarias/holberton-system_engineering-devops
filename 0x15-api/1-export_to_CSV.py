c8cf405bba34075d0dcd7d5391f6edb40ee99965
7honarias <7honarias@gmail.com>
2021-05-04 00:15:37 +0000

    [UPDATE] - add task 1 csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """script that using REST API"""
+import csv
 import requests
 from sys import argv
 
@@ -10,10 +11,11 @@ if __name__ == '__main__':
                         format(userId), verify=False).json()
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
                         format(userId), verify=False).json()
-    completed_tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+    file_csv = argv[1] + '.csv'
+    with open(file_csv, 'w') as csvfile:
+        txt = csv.writer(csvfile)
+        for task in todo:
+            txt.writerow([str(argv[1]),
+                          user.get('username'),
+                          task.get('completed'),
+                          task.get('title')])
