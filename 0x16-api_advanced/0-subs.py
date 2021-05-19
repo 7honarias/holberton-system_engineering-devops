d3d48561997ac29fb2ae7974717ae21ddf8dcc1c
7honarias <7honarias@gmail.com>
2021-05-19 13:14:11 +0000

    [UPDATE] - error when no exist

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,5 +9,7 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=user_agent)
     result = response.json()
-
-    return (result["data"]["subscribers"])
+    try:
+        return (result["data"]["subscribers"])
+    except:
+        return 0
