5d7f75cd1b3aef05cb847e8f693309cb47db86c7
7honarias <7honarias@gmail.com>
2021-05-19 13:09:03 +0000

    [UPDATE] - errors pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """ api reddit"""
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     user_agent = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=user_agent)
